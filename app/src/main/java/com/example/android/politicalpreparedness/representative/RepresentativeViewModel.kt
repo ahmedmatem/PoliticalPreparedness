b024bab2c6541bbd84832c332e5763df1df7cac2
Ahmed Matem Ahmed <ahmedmatem@gmail.com>
2021-09-07 06:53:15 +0100

    Implement RepresentativesViewModel

--- app/src/main/java/com/example/android/politicalpreparedness/representative/RepresentativeViewModel.kt
+++ app/src/main/java/com/example/android/politicalpreparedness/representative/RepresentativeViewModel.kt
@@ -1,12 +1,40 @@
 package com.example.android.politicalpreparedness.representative
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.android.politicalpreparedness.network.CivicsApi
+import com.example.android.politicalpreparedness.network.CivicsApiService
+import com.example.android.politicalpreparedness.network.models.Address
+import com.example.android.politicalpreparedness.representative.model.Representative
+import kotlinx.coroutines.launch
 
 class RepresentativeViewModel: ViewModel() {
 
+    private val apiService = CivicsApi.retrofitService
+
     //TODO: Establish live data for representatives and address
+    private val _representatives = MutableLiveData<List<Representative>>()
+    val representatives: LiveData<List<Representative>> = _representatives
+
+    private val _address = MutableLiveData<Address>()
+    val address: LiveData<Address> = _address
+
+    init {
+        _address.value = Address("", "", "", "", "")
+    }
 
     //TODO: Create function to fetch representatives from API from a provided address
+
+    fun getRepresentatives() {
+        viewModelScope.launch {
+            address.value?.let {
+                val (offices, officials) = apiService.getRepresentatives(it.toFormattedString())
+                _representatives.value = offices.flatMap { office ->  office.getRepresentatives(officials)}
+            }
+        }
+    }
 
     /**
      *  The following code will prove helpful in constructing a representative from the API. This code combines the two nodes of the RepresentativeResponse into a single official :
